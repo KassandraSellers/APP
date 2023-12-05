dc8e4e3f23c031d025e6c011a4dd6f959a8302fe
KassandraSellers <150406775+KassandraSellers@users.noreply.github.com>
2023-12-05 10:42:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -51,12 +51,72 @@ st.markdown("Are you a LinkedIn user?")
 
 st.markdown("Answer the questions to find out!")
 
-income_list = list["1" : "Less Than $10,000", "2":"10 to under $20,000", "3":"20 to under $30,000","4":"30 to under $40,000","5":"40 to under $50,000","6":"50 to under $75,000", "7":"75 to under $100,000","8":"100 to under $150,000","9":"$150,000 or more","10":"NA"]
-education_list = list["1":"Less than High School 1-8", "2":"No Diploma", "3":"High School Graduate", "4":"Some College, No Degree", "5":"Associate Degree","6":"Bachelor Degree","7":"Some postgraduate no degree","8":"Postgraduate or Professional Degree","9":"NA"]
-parent_list= list["0":"False", "1":"True"]
-married_list= list["0":"False", "1":"True"]
-gender_list= list["0":"Male", "1":"Female", "0":"Other"]
+income_list = list("1-Less Than $10,000", "2-10 to under $20,000", "3-20 to under $30,000","4-30 to under $40,000","5-40 to under $50,000","6-50 to under $75,000", "7-75 to under $100,000","8-100 to under $150,000","9-$150,000 or more")
+education_list = list("1-Less than High School 1-8", "2-No Diploma", "3-High School Graduate", "4-Some College, No Degree", "5-Associate Degree","6-Bachelor Degree","7-Some postgraduate no degree","8-Postgraduate or Professional Degree")
+parent_list= list("0 -False", "1-True")
+married_list= list("0-False", "1-True")
+gender_list= list("0-Male", "1-Female", "3-Other")
 
+#Income Variable Assignment
+    match income_list:
+        case "1-Less Than $10,000":
+            income = 1
+        case "2-10 to under $20,000":
+            income = 2
+        case "3-20 to under $30,000":
+            income = 3
+        case "4-30 to under $40,000":
+            income = 4
+        case "5-40 to under $50,000":
+            income = 5
+        case "6-50 to under $75,000":
+            income = 6
+        case "7-75 to under $100,000":
+            income = 7
+        case "8-100 to under $150,000":
+            income = 8
+        case "9-$150,000 or more":
+            income = 9
+
+
+match education_list:
+        case "1-Less than High School 1-8":
+            education = 1
+        case "2-No Diploma":
+            education = 2
+        case "3-High School Graduate":
+            education = 3
+        case "4-Some College, No Degree":
+            education = 4
+        case "5-Associate Degree":
+            education = 5
+        case "6-Bachelor Degree":
+            education = 6
+        case "7-Some postgraduate no degree":
+            education = 7
+        case "8-Postgraduate or Professional Degree":
+            education = 8
+
+match parent_list:
+        case "0 -False":
+            parent = 0
+        case "1-True":
+            parent = 1
+
+match married_list:
+        case "0 -False":
+            married = 0
+        case "1-True":
+            married = 1
+
+match gender_list:
+        case "0-Male":
+            gender = 0
+        case "1-Female":
+            gender = 1
+        case "3-Other":
+            gender = 0
+       
 user_income = st.selectbox(label = "What is your income?", options = income_list)
 
 user_education = st.selectbox(label = "What is your education level?", options = education_list)
@@ -76,7 +136,7 @@ user_complete = st.button("Calculate")
 if user_compete == True:
 
 #creating user data
-    User = pd.DataFrame(np.array([{user_income}, {user_education},{user_parent},{user_married},{user_female},{user_age}]).reshape(1,-1), 
+    User = pd.DataFrame(np.array([{income}, {education},{parent},{married},{female},{user_age}]).reshape(1,-1), 
                     columns=['income','education','parent','married','female','age'])
 #Predicting outcome
     model.predict(User)
